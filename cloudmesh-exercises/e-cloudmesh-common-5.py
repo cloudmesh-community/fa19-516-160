4dc548e3e35e73d9646ffb350cb70c062053e7e8
Shreyans Jain <shrejain@iu.edu>
2019-09-18 02:08:09 -0400

    e-cloudmesh-common-5 code added

---
+++ cloudmesh-exercises/e-cloudmesh-common-5.py
@@ -0,0 +1,30 @@
+# fa19-516-160
+# E.Cloudmesh.Common.5
+# Task :  Develop a program that demonstrates the use of cloudmesh.common.StopWatch.
+
+from cloudmesh.common.StopWatch import StopWatch # import StopWatch
+from time import sleep
+
+
+# Start timer
+StopWatch.start("calculation_sw")
+a=5
+b=6
+product = a*b
+print(product)
+sleep(1)
+
+sum=a+b
+
+print(sum)
+
+# Stop timer
+StopWatch.stop("calculation_sw")
+
+# Print results
+print (StopWatch.get("calculation_sw"))
+
+
+
+
+#Code reference Introduction to python : Gregor von Laszewski.: section 6.6
