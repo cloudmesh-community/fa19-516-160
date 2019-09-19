f402e6da9fa86a02488b15ce0019c951f0390592
Shreyans Jain <shrejain@iu.edu>
2019-09-19 00:08:03 -0400

    e-cloudmesh-common-1 code added

--- cloudmesh-exercises/E-Cloudmesh-Shell-1.py
+++ cloudmesh-exercises/E-Cloudmesh-Shell-1.py
@@ -2,9 +2,34 @@
 # E.Cloudmesh.Common.5
 # Task :  Install cmd5 and the command cms on your computer.
 
+from cloudmesh.common.Shell import Shell
 cms = Shell.execute('cms')
 print(cms)
 
 
 #Code reference Introduction to python : Gregor von Laszewski.: section 6.6
-# fa19-516-167
+
+
+#(ENV3) Shreyanss-MacBook-Pro:cm shreyansjain$ cms help
+
+#Documented commands (type help <topic>):
+#========================================
+#EOF     clear     default  help  man    plugin  quit  shell  stopwatch  var
+#banner  commands  echo     info  pause  q       set   sleep  sys        version
+
+#(ENV3) Shreyanss-MacBook-Pro:cm shreyansjain$ cms
+
+#+-------------------------------------------------------+
+#|   ____ _                 _                     _      |
+#|  / ___| | ___  _   _  __| |_ __ ___   ___  ___| |__   |
+#| | |   | |/ _ \| | | |/ _` | '_ ` _ \ / _ \/ __| '_ \  |
+#| | |___| | (_) | |_| | (_| | | | | | |  __/\__ \ | | | |
+#|  \____|_|\___/ \__,_|\__,_|_| |_| |_|\___||___/_| |_| |
+#+-------------------------------------------------------+
+#|                  Cloudmesh CMD5 Shell                 |
+#+-------------------------------------------------------+
+
+#cms> q
+#(ENV3) Shreyanss-MacBook-Pro:cm shreyansjain$
+
+#
